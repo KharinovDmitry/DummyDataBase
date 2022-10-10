8c0f8c4efbe5a4d514fd875533e0cea5ecb8aa78
Kharinov Dmitry <web-giorno@mail.ru>
2022-10-10 22:33:58 +0500

    Init

---
+++ DummyDataBase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DummyDataBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
