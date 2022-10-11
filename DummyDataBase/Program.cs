edb5dea5462359188d7e38d8e44566a87fff00db
Kharinov Dmitry <web-giorno@mail.ru>
2022-10-11 17:46:35 +0500

    TestData

--- DummyDataBase/Program.cs
+++ DummyDataBase/Program.cs
@@ -6,7 +6,21 @@ namespace DummyDataBase
     {
         static void Main(string[] args)
         {
-            
+            List<Book> books = new List<Book>()
+            {
+                new Book("Война и мир", "Толстой", 2020, 1, 1),
+                new Book("Преступление и наказание", "Достоевский", 2015, 1, 1)
+            };
+            List<Reader> readers = new List<Reader>()
+            {
+                new Reader(1, "Устьянцев Евгений Юрьевич"),
+                new Reader(2, "Кожурков Георгий Хорошкомалькович")
+            };
+            List<Action> actions = new List<Action>()
+            {
+                new Action(books[0], readers[0], new DateTime(2022, 10, 8)),
+                new Action(books[1], readers[1], new DateTime(2022, 10, 9), new DateTime(2022, 10, 11))
+            };
         }
     }
 }
