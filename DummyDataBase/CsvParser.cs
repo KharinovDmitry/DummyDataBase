126952d522ea8e00a1faa6b25beca47414fce048
Kharinov Dmitry <web-giorno@mail.ru>
2022-10-13 02:38:52 +0500

    Data validation

--- DummyDataBase/CsvParser.cs
+++ DummyDataBase/CsvParser.cs
@@ -18,8 +18,56 @@
 
         private static bool isValid(string input, JsonTable format)
         {
+            bool res = true;
             string[] inputLines = input.Split("\n");
             string[] titles = inputLines[0].Split(";");
+            res = isValidTitles(format, titles);
+            res = isValidData(format, inputLines);
+            return res;
+        }
+
+        private static bool isValidData(JsonTable format, string[] inputLines)
+        {
+            bool res = true;
+            for(int s = 1; s < inputLines.Length; s++)
+            {
+                string[] fields = inputLines[s].Split(";");
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    switch (format.Fields[i].Type)
+                    {
+                        case "int":
+                            if(!int.TryParse(fields[i], out int temp))
+                            {
+                                res = false;
+                                WriteError(s, i, format.Fields[i].Type, fields[i]);
+                            }
+                            break;
+                        case "DateTime":
+                            if(!DateTime.TryParse(fields[i], out DateTime temp2))
+                            {
+                                res = false;
+                                WriteError(s, i, format.Fields[i].Type, fields[i]);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return res;
+        }
+
+        private static void WriteError(int s, int i, string typeFormat, string typeExept)
+        {
+            Console.WriteLine("Неверный формат данный\n" +
+                "строка: {0} столбец: {1} - Ожидалось {2}, а встречено \"{3}\"",
+                s + 1, i + 1, typeFormat, typeExept);
+        }
+
+
+        private static bool isValidTitles(JsonTable format, string[] titles)
+        {
             if (titles.Length != format.Fields.Count)
             {
                 Console.WriteLine("Ожидалось {0} столбцов, а получено {1}",
@@ -28,24 +76,12 @@
             }
             for (int i = 0; i < titles.Length; i++)
             {
-                Console.WriteLine("[{0}] [{1}]", titles[i], format.Fields[i].Name);
                 if (!titles[i].Contains(format.Fields[i].Name))
                 {
                     Console.WriteLine("Несоответсвие заголовков\n" +
                         "Столбец:{0} Ожидалось {1}, а встречено {1}",
                         i + 1, format.Fields[i].Name, titles[i]);
                     return false;
-                }
-            }
-
-            string[] readersStrings = new string[inputLines.Length];
-            inputLines.CopyTo(readersStrings, 0);
-            foreach (var readerString in readersStrings)
-            {
-                string[] fields = readerString.Split(";");
-                for (int i = 0; i < fields.Length; i++)
-                {
-                    
                 }
             }
             return true;
