edeb1aeb95d65cc6691ddb2aae1346159180845f
Kharinov Dmitry <web-giorno@mail.ru>
2022-10-12 22:13:11 +0500

    Refactor Action class

--- DummyDataBase/Book.cs
+++ DummyDataBase/Book.cs
@@ -9,13 +9,19 @@
         public int Wardrobe { get; set; }
         public int Shelf { get; set; }
 
-        public Book(string name, string author, int yearPublication, int wardrobe, int shelf)
+        public Book(int id, string name, string author, int yearPublication, int wardrobe, int shelf)
         {
+            ID = id;
             Name = name;
             Author = author;
             YearPublication = yearPublication;
             Wardrobe = wardrobe;
             Shelf = shelf;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", ID, Name, Author, YearPublication, Wardrobe, Shelf);
         }
     }
 }
--- DummyDataBase/Reader.cs
+++ DummyDataBase/Reader.cs
@@ -1,14 +1,21 @@
-﻿namespace DummyDataBase
+﻿using System.Xml.Linq;
+using System;
+
+namespace DummyDataBase
 {
     class Reader
     {
         public int ID { get; }
         public string FullName { get; set; }
 
-        public Reader(int iD, string fullName)
+        public Reader(int id, string fullName)
         {
-            ID = iD;
+            ID = id;
             FullName = fullName;
+        }
+        public void Print()
+        {
+            Console.WriteLine("{0} {1}", ID, FullName);
         }
     }
 }
