c7c5782d477211da30d4135168fd0e73d4c37837
Kharinov Dmitry <web-giorno@mail.ru>
2022-10-18 17:44:54 +0500

    final laba 3

--- DummyDataBase/Program.cs
+++ DummyDataBase/Program.cs
@@ -24,34 +24,102 @@ namespace DummyDataBase
             if (readers == null || books == null || history == null)
                 return;
 
-            foreach (var book in books)
-            {
-                Console.WriteLine("{0} \"{1}\" г.{2}",book.Author, book.Name, book.YearPublication);
-                string info = GetInfoBook(book, history);
-                if (info != "")
-                    Console.WriteLine("\t" + info);
-                Console.WriteLine();
-            }
+            PrintBookInfo(books, history);
 
             Console.ReadKey();
         }
 
-        private static string GetInfoBook(Book book, List<Action> history)
+        private static void PrintBookInfo(List<Book> books, List<Action> history)
         {
-            string res = "";
+            int lengthAuthor = 9;
+            int lengthBookName = 10;
+            int lengthReader = 8;
+            int lengthDate = 10;
+            foreach (var book in books)
+            {
+                lengthAuthor = Math.Max(lengthAuthor, book.Author.Length);
+                lengthBookName = Math.Max(lengthBookName, book.Name.Length);
+                Action lastAction = GetLastTakeAction(book, history);
+                if (lastAction != null)
+                    lengthReader = Math.Max(lengthReader, lastAction.Reader.FullName.Length);
+                else
+                    lengthReader = lengthReader;
+            }
+            PrintTitle(lengthAuthor, lengthBookName, lengthReader, lengthDate);
+            foreach (var book in books)
+            {
+                Console.Write($"| {book.Author} ");
+                for (int i = 0; i < lengthAuthor - book.Author.Length; i++)
+                    Console.Write(" ");
+                Console.Write($"| {book.Name} ");
+                for (int i = 0; i < lengthBookName - book.Name.Length; i++)
+                    Console.Write(" ");
+                Action lastAction = GetLastTakeAction(book, history);
+                if(lastAction != null)
+                {
+                    Console.Write($"| {lastAction.Reader.FullName} ");
+                    for (int i = 0; i < lengthReader - lastAction.Reader.FullName.Length; i++)
+                        Console.Write(" ");
+                    Console.Write($"| {lastAction.Date.Day}.{lastAction.Date.Month}.{lastAction.Date.Year} ");
+                    Console.WriteLine("|");
+                }
+                else
+                {
+                    Console.Write($"|");
+                    for (int i = 0; i < lengthReader + 2; i++)
+                        Console.Write(" ");
+                    Console.Write($"|");
+                    for (int i = 0; i < lengthDate + 2; i++)
+                        Console.Write(" ");
+                    Console.WriteLine("|");
+                }
+            }
+        }
+
+        private static void PrintTitle(int lengthAuthor, int lengthBookName, int lengthReader, int lengthDate)
+        {
+            Console.Write("| Автор ");
+            for (int i = 0; i < lengthAuthor - 5; i++)
+                Console.Write(" ");
+            Console.Write("| Название ");
+            for (int i = 0; i < lengthBookName - 8; i++)
+                Console.Write(" ");
+            Console.Write("| Читает ");
+            for (int i = 0; i < lengthReader - 6; i++)
+                Console.Write(" ");
+            Console.Write("| Взял ");
+            for (int i = 0; i < lengthDate - 4; i++)
+                Console.Write(" ");
+            Console.WriteLine("|");
+
+            Console.Write("|");
+            for (int i = 0; i < lengthAuthor + 2; i++)
+                Console.Write("-");
+            Console.Write("|");
+            for (int i = 0; i < lengthBookName + 2; i++)
+                Console.Write("-");
+            Console.Write("|");
+            for (int i = 0; i < lengthReader + 2; i++)
+                Console.Write("-");
+            Console.Write("|");
+            for (int i = 0; i < lengthAuthor + 2; i++)
+                Console.Write("-");
+            Console.WriteLine("|");
+        }
+
+        private static Action GetLastTakeAction(Book book, List<Action> history)
+        {
+            Action res = null;
             foreach (var action in history)
             {
                 if(action.Book == book)
                 {
-                    if (action.TypeAction.Contains("Взял"))
-                    {
-                        res = $"Взял {action.Reader.FullName} {action.Date.Day}.{action.Date.Month}.{action.Date.Year}";
-                    }
+                    if (action.TypeAction == "Взял")
+                        res = action;
                     else
-                    {
-                        res = "";
-                    }
+                        res = null;
                 }
+                
             }
             return res;
         }
